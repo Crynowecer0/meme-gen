253dba7c7ec354e7d4b6993e0029fb1245b0192b
Crynowecer0 <crynowecer11@gmail.com>
2023-03-26 16:36:26 -0400

    progress on generateMeme - successfully append unstyled image element to DOM

--- scripts.js
+++ scripts.js
@@ -8,15 +8,17 @@ function generateMeme() {
   const submitter = document.getElementById("submit-button");
   const formData = new FormData(form, submitter);
 
+  const backgroundImg = 'https://www.ultimate-leadership-training.co.uk/images/xshit_down.jpg.pagespeed.ic.fO-EV_o3OU.webp'
+
   //generate a blank dom element -> seperate function most likely
-  const newMeme = document.createElement("div");
+  const newMeme = document.createElement("img");
+  newMeme.src = backgroundImg;
   const memeContent = document.createTextNode('hello world!')
   newMeme.appendChild(memeContent)
   newMeme.classList.add('meme')
   //add the new meme to the dom
   const memeContainer = document.getElementById('meme-container')
   memeContainer.appendChild(newMeme)
-
 
   //append the data from the form fields to the appropriate places
   //on the generated dom element
