1823d3134d3404f388a2a5dfb35fd8f43cff3d62
Crynowecer0 <crynowecer11@gmail.com>
2023-03-26 16:05:30 -0400

    generate meme function is able to access form data, and append a template div to the dom

--- scripts.js
+++ scripts.js
@@ -1,10 +1,26 @@
-
-
 const form = document.getElementById("meme-entry-form");
 form.addEventListener("submit", generateMeme);
 // submitButton.addEventListener("click", generateMeme);
 
 function generateMeme() {
-  console.log("hello");
+  //access the data in the form fields
+  const form = document.getElementById("meme-entry-form");
+  const submitter = document.getElementById("submit-button");
+  const formData = new FormData(form, submitter);
+
+  //generate a blank dom element -> seperate function most likely
+  const newMeme = document.createElement("div");
+  const memeContent = document.createTextNode('hello world!')
+  newMeme.appendChild(memeContent)
+  newMeme.classList.add('meme')
+  //add the new meme to the dom
+  const memeContainer = document.getElementById('meme-container')
+  memeContainer.appendChild(newMeme)
+
+
+  //append the data from the form fields to the appropriate places
+  //on the generated dom element
+  //append the generated element to the dom
+
   event.preventDefault();
 }
